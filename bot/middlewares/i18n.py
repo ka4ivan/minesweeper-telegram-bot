83000c2e39e236de64f8deceb3d6bb87d9e2733d
Kachula Ivan <ka4ivan.dev@gmail.com>
2026-01-21 20:55:03 +0200

    i18n

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -12,24 +12,23 @@ pybabel compile -d bot/locales -D messages --statistics
 pybabel update -i bot/locales/messages.pot -d bot/locales -D messages
 
 """
-from typing import Callable
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
 from bot.utils.i18n import i18n, SUPPORTED_LOCALES
 
 class I18nMiddleware(BaseMiddleware):
-    async def __call__(self, handler: Callable, event: TelegramObject, data: dict):
+    async def __call__(self, handler, event, data):
         user = getattr(event, "from_user", None)
 
-        locale = user.language_code.split("-")[0] if user and user.language_code else i18n.default_locale
+        locale = (
+            user.language_code.split("-")[0]
+            if user and user.language_code
+            else i18n.default_locale
+        )
+
         if locale not in SUPPORTED_LOCALES:
             locale = i18n.default_locale
 
         data["locale"] = locale
-        print(locale) # 'uk'
-        i18n.use_locale(locale)
-        print(i18n.locales)  # {'ru': <gettext.GNUTranslations object at 0x74c1ef2c4fe0>, 'uk': <gettext.GNUTranslations object at 0x74c1eee77230>, 'en': <gettext.GNUTranslations object at 0x74c1eee773e0>}
-        print(i18n.current_locale)  # 'en'
-        print(locale)  # 'uk'
 
-        return await handler(event, data)
+        with i18n.use_locale(locale):
+            return await handler(event, data)
