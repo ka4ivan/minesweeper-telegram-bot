4fa2daab3005db8c914ec1f892c0b88b43d65daa
Kachula Ivan <ka4ivan.dev@gmail.com>
2026-01-22 23:05:52 +0200

    redis env

--- bot/dependencies.py
+++ bot/dependencies.py
@@ -1,8 +1,9 @@
 from redis.asyncio import Redis
 from bot.repositories.redis_repository import RedisRepository
 from bot.services.game_service import GameService
+import os
 
-redis = Redis(host="redis", port=6379, decode_responses=True)
+redis = Redis(host=os.getenv("REDIS_HOST"), port=int(os.getenv("REDIS_PORT")), decode_responses=True)
 
 redis_repo = RedisRepository(redis)
 
